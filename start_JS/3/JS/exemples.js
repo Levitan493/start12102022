5c1c8632248d457f34be27cfbc49f9df923df71e
Levitan493 <levitan493@gmail.com>
2022-12-24 00:40:30 +0200

    Bootstrap

--- start_JS/3/JS/exemples.js
+++ start_JS/3/JS/exemples.js
@@ -1,8 +1,9 @@
-function printArg(value = 'Empty', printTime){
-    if(printTime === undefined){
-        printTime = 3;
-    }
-    for(let i = 0; i<printTime; i++){
-        console.log(value);
-    }
-}
+// function printArg(value = 'Empty', printTime){
+//     if(printTime === undefined){
+//         printTime = 3;
+//     }
+//     for(let i = 0; i<printTime; i++){
+//         console.log(value);
+//     }
+// }
+
