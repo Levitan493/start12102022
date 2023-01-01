8e95fa038f0ab1b05f6f4cefd122a824ebb95327
Levitan493 <levitan493@gmail.com>
2023-01-01 19:56:18 +0200

    JS4

--- start_JS/3/JS/exemples.js
+++ start_JS/3/JS/exemples.js
@@ -30,3 +30,27 @@
 // else{
 //     alert('Hello stranger');
 // }
+//  let sheepCounter = 0;
+//  while(shipCounter<100){
+//     console.log('I have' + sheepCounter + 'sheep!');
+//     sheepCounter++;
+//  }
+//  console.log('Zzzzzzzz');
+
+//  let userName = prompt('What is yor name');
+// if(userName ==='Ivan'){
+//     alert('Hello me' + '!');
+// }
+// else if(userName === 'Solia'){
+//     alert('Hello ' + userName + '!');
+// }
+// else if(userName === 'Diana'){
+//     alert('Hello ' + userName + '!');
+// }
+// else{
+//     alert('Hello stranger' + '!');
+// }
+
+// for(let x=2; x<10000; x=x*2){
+//     console.log(x);
+// }
