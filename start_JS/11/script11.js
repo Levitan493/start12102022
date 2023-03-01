f290a58975477d5b0eeddf76186f420dcadee12e
Levitan493 <levitan493@gmail.com>
2023-03-01 14:23:49 -0600

    HW_11

--- start_JS/11/script11.js
+++ start_JS/11/script11.js
@@ -43,24 +43,69 @@ window.onload = function () {
     // "12345"  -->  false
     // "a234"   -->  false
 
-    // const body = document.querySelector('body');
-    // const inp = document.createElement('')
+    const form = document.querySelector('.My_form');
+    const input = document.querySelector('input');
+    const SubmitButton = document.querySelector('.SubmitButton');
+    SubmitButton.style.width = '50px';
+    SubmitButton.style.margin = '50px auto';
+    SubmitButton.style.backgroundColor = 'rgb(19, 173, 144)';
+    SubmitButton.style.borderRadius = '10px';
+    SubmitButton.value = 'Submit';
+    SubmitButton.style.padding = '5px 3px';
+    form.style.display = 'flex';
+    form.style.border = '1px solid black';
+    form.style.flexDirection = 'column';
+    form.style.width = '300px';
+    form.style.height = '400px';
+    form.style.margin = '50px auto';
+    form.style.backgroundColor = 'rgba(225, 210, 234, 0.71)';
+    input.style.margin = '50px auto';
+    input.style.border = '1px solid black'
+    input.style.padding = '10px';
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        const validPass = input.value;
+        const patternPass = /^1234$/;
+        if(patternPass.test(validPass)){
+            alert('True')
+        }
+        else{
+            alert('False');
+            
+            
+        }
+    })
+    
 
 
     // =======================================================================================================
     // 4) Створити регулярку яка отримує адресс сторінки і повертає тільки адресс  
     // "https://prog.academy/?page=1" -->"https://prog.academy/"
 
-    const returnAdresss = function(linc){
-        const checkAdress = linc.math()
+    const returnAdresss = function (linc) {
+        const checkAdress = linc.replace(/\?.*/, '');
+        console.log(checkAdress);
     }
 
+    returnAdresss("https://prog.academy/?page=1");
     // ===========================================================================================================
-    // 5) Створити функцію валідації юзернейма, імя може включати літери цифри, тире і нижьнє тире, довжина імя повина бути від 4 до 10 символів. Приклад: validateUsr('a') => false,
-    // validateUsr('p1pp1') => true, validateUsr('asd43_34') => true
+    // 5) Створити функцію валідації юзернейма, імя може включати літери цифри, тире і нижьнє тире, довжина імя повина бути від 4 до 10 символів. Приклад: validateUsr('asd43_34') => false,
+    // validateUsr('asd43_34') => true, validateUsr('asd43_34') => true
 
+    let validateUsr = (username) => {
+        let isValidPatern = /^[\w-]{4,10}$/
+        if (isValidPatern.test(username)) {
+            console.log(true)
+        }
+        else {
+            console.log(false);
+        }
 
-
+    }
+    validateUsr('a');
+    validateUsr('p1pp1');
+    validateUsr('asd.43_34');
 
 
 }
