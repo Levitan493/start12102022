6ec91386a4defbea09d9c2c388fee8b911612e6d
Levitan493 <levitan493@gmail.com>
2023-03-03 12:32:38 -0600

    hw12

---
+++ start_JS/12/js/canvas_circle.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+    // - Створити функцію яка малює коло в канвасі і розміщує в середині кола текст 
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const centerX = canvas.width/2;
+const centerY = canvas.height/2;
+canvas.style.margin = '50px auto';
+canvas.style.border = '2px solid red';
+
+
+function drawCircle(x, y, radius, text){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.font = ('26px Arial');
+    ctx.strokeText(text, centerX - radius / 2, centerY);
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'center';
+}
+
+drawCircle(centerX, centerY, 100, 'This circle');
+
+}
